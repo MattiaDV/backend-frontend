12656df8050326ca89bdb1644e8c419dd03803b3
MattiaDV <mattiadv25105@gmail.com>
2025-05-22 16:46:57 +0200

    a

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ import http from 'http';
 import { Server } from 'socket.io';
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Crea server HTTP da Express
 const server = http.createServer(app);
@@ -56,5 +56,5 @@ io.on('connection', (socket) => {
 
 // Fai partire il server HTTP (non app.listen)
 server.listen(port, "127.0.0.1", () => {
-    console.log(`Listening on https://backend-frontend-xtk4.onrender.com`);
+    console.log(`Listening on https://backend-frontend-xtk4.onrender.com:${port}`);
 });

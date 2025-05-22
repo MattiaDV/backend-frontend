ba148ffea4e2b17ecdc6525335d4baa15cc94251
MattiaDV <mattiadv25105@gmail.com>
2025-05-22 16:38:17 +0200

    a

--- script/script.js
+++ script/script.js
@@ -1,15 +1,47 @@
-let login = document.getElementById('login');
-let signin = document.getElementById('signin');
-let home = document.getElementById('home');
+let adminMessages = document.getElementById('admin');
 
-home.addEventListener('click', function() {
-    window.location.href = "/index";
-})
+fetch('../pages/messages.json')
+  .then(res => res.json())
+  .then(messages => {
+    if(messages.length > 0) {
+      adminMessages.innerHTML = `
+      
+        <div class = "messaggiocompleto">
+        <div class="nickname">${messages[0].nickname}</div>
+        <div class="message">${messages[0].messaggio}</div>
+        </div>
+      `;
+    } else {
+      adminMessages.innerHTML = '<p>Nessun messaggio disponibile</p>';
+    }
+  })
+  .catch(err => {
+    console.error('Errore nel caricamento dei messaggi:', err);
+    adminMessages.innerHTML = '<p>Errore nel caricamento dei messaggi</p>';
+  });
 
-login.addEventListener('click', function() {
-    window.location.href = "/login";
-})
 
-signin.addEventListener('click', function() {
-    window.location.href = "/signin";
-})
+let chatg = document.getElementById('chatGene');
+
+fetch('../pages/messages.json')
+  .then(res => res.json())
+  .then(messages => {
+    if(messages.length > 0) {
+      let html = '';
+      for (let i = 0; i < messages.length; i++) {
+        html += `
+          <div class = "messaggiocompleto">
+          <div class="nickname">${messages[i].nickname}</div>
+          <div class="message">${messages[i].messaggio}</div>
+          </div>
+        `;
+      }
+      chatg.innerHTML = html;
+    } else {
+      chatg.innerHTML = '<p>Nessun messaggio disponibile</p>';
+    }
+  })
+  .catch(err => {
+    console.error('Errore nel caricamento dei messaggi:', err);
+    chatg.innerHTML = '<p>Errore nel caricamento dei messaggi</p>';
+  });
--- server/server.js
+++ server/server.js
@@ -1,34 +1,60 @@
 import express from 'express';
 import getPages from '../getPages.js';
-const app = express()
+import { readFile, writeFile } from 'fs/promises';
+import http from 'http';
+import { Server } from 'socket.io';
+
+const app = express();
 const port = 3000;
 
+// Crea server HTTP da Express
+const server = http.createServer(app);
+
+// Inizializza Socket.IO sul server HTTP
+const io = new Server(server);
+
 app.use(express.static('./'));
-
-
-
-
+app.use(express.json());
 
 app.get('/', async (req, res) => {
     getPages.getIndex(req, res);
-})
+});
 
 app.get('/index', async (req, res) => {
     getPages.getIndex(req, res);
-})
+});
 
-app.get('/login', async (req, res) => {
-    getPages.getLogin(req, res);
-})
+app.post('/messaggio', async (req, res) => {
+    try {
+        const data = await readFile('./pages/messages.json', 'utf8');
+        const messages = JSON.parse(data);
 
-app.get('/signin', async (req, res) => {
-    getPages.getSignin(req, res);
-})
+        messages.push(req.body);
 
+        await writeFile('./pages/messages.json', JSON.stringify(messages, null, 2), 'utf8');
 
+        // Manda il messaggio a tutti i client connessi via socket
+        io.emit('newMessage', req.body);
 
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('Messaggio aggiunto con successo!');
+    } catch (err) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        console.log("Errore: ", err);
+        res.end('Errore del server interno');
+    }
+});
 
+// Socket.IO: gestione connessioni
+io.on('connection', (socket) => {
+    console.log('Utente connesso:', socket.id);
 
-app.listen(port, "127.0.0.1", () => {
-    console.log("Listening to 127.0.0.1");
+    socket.on('disconnect', () => {
+        console.log('Utente disconnesso:', socket.id);
+    });
+});
+
+// Fai partire il server HTTP (non app.listen)
+server.listen(port, "127.0.0.1", () => {
+    console.log(`Listening on http://127.0.0.1:${port}`);
 });
